a0ce3551f7d6b482c56fe48df5330853e0dfa460
Francisco Javier Parga Garcia <fgarcia24@slb.com>
2024-05-12 13:58:51 -0600

    Ambient detection

---
+++ librerias_integrador/utils.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+def verificar_ambiente():
+    # Verificar si está en Google Colab
+    if 'google.colab' in sys.modules:
+        return "Estás trabajando en Google Colab."
+    # Verificar si está en un sistema Windows
+    elif os.name == 'nt':
+        print("Estás trabajando en un sistema Windows.")
+        return 'win'
+    else:
+        print("El ambiente de trabajo no es Google Colab ni Windows.")
+        return 'colab'
